98e92188e4855419c0c6c632362fa2caa5c5e2a3
Andriy Kikot <andray838@gmail.com>
2020-10-11 16:35:04 +0300

    added finished homework-12

--- src/index.js
+++ src/index.js
@@ -1,8 +1,29 @@
 import './styles.css';
+import fetchCountries from './js/fetchCountries.js';
+import renderCountriesMarkup from './js/renderCountriesMarkup.js';
+import { refs } from './js/refs';
+import error from './js/pnotify.js'
+
+const debounce = require('lodash.debounce');
+
+refs.searchInput.addEventListener('input', debounce(searchInputHandler, 500));
 
 
+function searchInputHandler(event) {
+    let inputValue = event.target.value;
 
-fetch(`https://restcountries.eu/rest/v2/all`)
-    .then(response => response.json())
-    .then(data => console.log(data));
+    refs.countriesContainer.innerHTML = '';
+    refs.countriesList.innerHTML = '';
 
+    if (inputValue.length > 1) {
+        fetchCountries(inputValue)
+            .then(data => {
+                if (data.length > 0 && data.length <= 10) {
+                    renderCountriesMarkup(data);
+                } else if (data.length > 10) {
+                    error('Too many matches found. Please enter a more specific query!');
+                }
+            })
+            .catch(error => console.log(error));
+    }
+}
